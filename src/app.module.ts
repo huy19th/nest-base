fa412005cd39d6c44e4c5b9276517a9b62550236
huynguyen <huy19th@gmail.com>
2024-05-22 23:35:22 +0700

    mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { configuration } from './config/configuration';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseConfigService } from './config/mongoose.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ load: [configuration] }),
+    MongooseModule.forRootAsync({ useClass: MongooseConfigService, imports: [ConfigModule] }),
   ],
   controllers: [AppController],
   providers: [],
--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -6,6 +6,9 @@ const env = process.env;
 
 const config: Configuration = {
     port: +env.PORT || 8080,
+    mongo: {
+        uri: process.env.MONGO_URI
+    },
 };
 
 export const configuration: ConfigFactory<Configuration> = () => config;
