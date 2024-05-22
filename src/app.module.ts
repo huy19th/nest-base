347a033dd4cf3caa6e88c4ef2b48693b69641637
huynguyen <huy19th@gmail.com>
2024-05-23 00:47:47 +0700

    user

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,14 @@ import { configuration } from './config/configuration';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MongooseConfigService } from './config/mongoose.config';
 import { CacheModule } from './providers/cache/cache.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ load: [configuration] }),
     MongooseModule.forRootAsync({ useClass: MongooseConfigService, imports: [ConfigModule] }),
     CacheModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [],
---
+++ src/modules/user/user.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersController } from './user.controller';
+import { UserService } from './user.service';
+import { User, UserSchema } from './user.schema';
+
+@Module({
+    imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
+    controllers: [UsersController],
+    providers: [UserService],
+    exports: [UserService]
+})
+export class UserModule { }
