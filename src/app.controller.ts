8fd0660bfada770782a199ba3751873b78d7008f
huynguyen <huy19th@gmail.com>
2024-05-23 21:56:30 +0700

    winston

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,17 @@
 import {
   Controller,
   Get,
+  Logger,
 } from '@nestjs/common';
 
 @Controller()
 export class AppController {
+  private logger: Logger = new Logger(AppController.name, { timestamp: true });
   constructor() { }
 
   @Get()
   getHello(): string {
+    this.logger.log("random log");
     return "Hello World!";
   }
 }
