5d978521be9c9a859f35b0693493d2ac32cef60c
huynguyen <huy19th@gmail.com>
2024-05-23 22:35:52 +0700

    memory storage

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { configuration } from './config/configuration';
+import { UploadModule } from './modules/upload/upload.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ load: [configuration] }),
+    UploadModule,
   ],
   controllers: [AppController],
   providers: [],
