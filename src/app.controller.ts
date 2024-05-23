119ec4395e8ee619ebd427e9d10cff61b1bad9da
huynguyen <huy19th@gmail.com>
2024-05-23 21:03:32 +0700

    throttler

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,11 @@
 import {
   Controller,
   Get,
+  UseGuards,
 } from '@nestjs/common';
+import { ThrottlerGuard } from '@nestjs/throttler';
 
+@UseGuards(ThrottlerGuard)
 @Controller()
 export class AppController {
   constructor() { }
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { configuration } from './config/configuration';
+import { ThrottlerConfigService } from './config/throttler.config';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ load: [configuration] }),
+    ThrottlerModule.forRootAsync({ useClass: ThrottlerConfigService, imports: [ConfigModule] }),
   ],
   controllers: [AppController],
-  providers: [],
+  providers: [
+    // { provide: APP_GUARD, useClass: ThrottlerGuard }, // bind throttle global scope
+  ],
 })
 export class AppModule { }
