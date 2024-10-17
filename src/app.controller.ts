6aaa6ef34745c9961503e42815c8a0f0ff4996d0
huynguyen <huy19th@gmail.com>
2024-10-17 21:36:14 +0700

    feat: mvc with template engine ejs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
 import {
   Controller,
   Get,
+  Render,
 } from '@nestjs/common';
 
 @Controller()
@@ -8,7 +9,8 @@ export class AppController {
   constructor() { }
 
   @Get()
-  getHello(): string {
-    return "Hello World!";
+  @Render('index')
+  getHello() {
+    return { message: `A progressive <a href="http://nodejs.org" rel="nofollow">Node.js</a> framework for building efficient and scalable server-side applications.` };
   }
 }
